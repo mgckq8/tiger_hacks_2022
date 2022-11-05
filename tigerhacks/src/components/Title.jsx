f30f13c3cc80b9fc2241a7c72e37556df13ce28f
jamesshawhan <jrshawhan@gmail.com>
2022-11-04 20:47:14 -0500

    Got rid of bloatware

---
+++ tigerhacks/src/components/Title.jsx
@@ -0,0 +1,10 @@
+import './Title.css'
+
+
+export default function Title(){
+    return(
+        <div className="title">
+            <p className='titletext'>RoadTrip Generator</p>
+        </div>
+    )
+}
